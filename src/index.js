cab92a8c66e8c06d736458f9346d99c24d2b125f
Manan Patel <mananpatel@192-168-1-103.tpgi.com.au>
2020-07-26 17:21:02 +1000

    remember the click

--- src/index.js
+++ src/index.js
@@ -4,9 +4,15 @@ import './index.css';
 
  // in this code we have 3 react components
 class Square extends React.Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            value: null,
+        };
+    }
   render() {
     return (
-      <button className="square" onClick= {function(){alert('click');}}>
+      <button className="square" onClick= {()=> alert('click')}>
         {this.props.value}
       </button>
     );
